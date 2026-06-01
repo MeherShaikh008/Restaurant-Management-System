56330a9c84f30ab249e976a751c605140f4a847c
MeherShaikh008 <shaikhmeher131@gmail.com>
2026-06-01 16:02:57 +0530

    fix deploy files

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)=3nu8=5w7&awxzde0=3q4+7w0((*62(ltohsybbnyo#n!giiq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG =True
+DEBUG =False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
